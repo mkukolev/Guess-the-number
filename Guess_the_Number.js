8ca1502df8abe9e5ee3e50fe9b6909c5963bc73f
Mikhail <michael.kukolev.phone@gmail.com>
2018-02-18 23:45:51 +0300

    change files
    .js, .html, .css

--- Guess_the_Number.js
+++ Guess_the_Number.js
@@ -2,16 +2,27 @@
 //var imgWTF = "https://cdn1.iconfinder.com/data/icons/photo-stickers-words/128/word_2-128.png";
 //var img404 = "https://cdn1.iconfinder.com/data/icons/photo-stickers-words/128/word_18-128.png";
 //var prg
+//persNumber = 6
+prgNumber = Math.floor((Math.random() * 10) + 1);
+console.log(prgNumber);
+persNumber =  Math.floor((Math.random() * 10) + 1);
+console.log(persNumber);
 
-prg = Math.floor((Math.random() * 10) + 1);
-console.log(prg);
-
-document.onclick = function(event) {
+/*document.onclick = function(event) {
     if (event.target.tagName == "IMG"); {
         event.target.classList.add('bordered');
     }
 }
-
+*/
+document.onclick = function(looseOrWin) {
+    if (looseOrWin.target.getAttribute('data-title') == "img " + persNumber) {
+            alert("Winner");
+    }
+        else (prgNumber != persNumber); {
+        alert("Another number");
+    } 
+}
+ /*
  window.onload = function() {
     var imgs = document.getElementsByTagName('IMG');
     for (var i in imgs) {
@@ -20,4 +31,5 @@ document.onclick = function(event) {
         }
     }
   }   
+  */
 

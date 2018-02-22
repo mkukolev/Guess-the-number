d65158a9bc972178ad45ae690825ec7ed2893103
mkukolev <michael.kukolev.phone@gmail.com>
2018-02-22 15:19:57 +0300

    logick done
    fix js html (class) css

--- Guess_the_Number.js
+++ Guess_the_Number.js
@@ -3,7 +3,7 @@
 
 // Создание рандомного числа, которое по сути и нужно будет угадать
 var prgNumber = Math.floor((Math.random() * 10) + 1);   
-var imglink = "pictures/1.png";
+//var imglink = "pictures/1.png";
 //debug
 console.log(prgNumber);
 
@@ -15,34 +15,51 @@ function reloadPage ()
         location.reload();
     }, 250)
 }
+// Массив с картинками
+var imglinks=
+[
+    'pictures/1.png',
+    'pictures/2.png',
+    'pictures/3.png',
+    'pictures/4.png',
+    'pictures/5.png',
+    'pictures/6.png',
+    'pictures/7.png',
+    'pictures/8.png',
+    'pictures/9.png',
+    'pictures/10.png'
+];
+//Определяем переменной класс в html
+var elemNum = document.getElementsByClassName('elemNum')[0];
 
-const img = document.querySelector('img');
-
-//Основная логика угадывания числа, и перезагрузка после попытки
-document.onclick = function(resultGame)
-
+//Присваиваем массив к классу,   
+for (var i in imglinks)
 {
-    if (resultGame.target.getAttribute('data-title') == "pic" + prgNumber) 
-    {
-        alert("You are winner!\n\t" + "Lets try again\t");
+    var images = '<img src = "'+ imglinks[i] +'"data-title =  "pic'+ (Number(i) + 1) + '" >';
+    elemNum.innerHTML += images;
+}
+//Клиекание по элементам в классе
+elemNum.onclick = function(event)
+{
+        //Основная логика угадывания числа, и перезагрузка после попытки
+        if (event.target.getAttribute('data-title') == "pic" + prgNumber)
+        
+        {
+            alert("You are winner!\n\t" + "Lets try again\t");
+        }
 
-    }
-
-    else if (resultGame.target.getAttribute('data-title') != "pic" + prgNumber)
-    {
-        alert("Sorry, you not guess the Nubmer! Try again.\t");
-    } 
-
-    else (document.target.getElementsByTagName("IMG") == "IMG")
-    {
-        resultGame.target.classList.add('bordered');
-        reloadPage();
-    }
-
+        else if (event.target.getAttribute('data-title') != "pic" + prgNumber)
+        {   
+            alert("Sorry, you not guess the Nubmer! Try again.\t");
+        } 
+        
+        { 
+            event.target.classList.add('bordered');
+            reloadPage();
+        }
 }
 
-console.log(document.getElementById("img1").tagName);
 
 
-console.log(document.getElementsByTagName('IMG'));
-console.log(imglink);
+
+

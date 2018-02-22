ba9cf287a1706a62668beacb4b1888be12f4bd3e
mkukolev <michael.kukolev.phone@gmail.com>
2018-02-22 11:57:30 +0300

    checkin
    fix array

--- Guess_the_Number.js
+++ Guess_the_Number.js
@@ -16,11 +16,8 @@ function reloadPage ()
     }, 250)
 }
 
-<<<<<<< HEAD
 const img = document.querySelector('img');
 
-=======
->>>>>>> 2c1db683410d3ac61f98935647c50188279e8403
 //Основная логика угадывания числа, и перезагрузка после попытки
 document.onclick = function(resultGame)
 
@@ -28,10 +25,7 @@ document.onclick = function(resultGame)
     if (resultGame.target.getAttribute('data-title') == "pic" + prgNumber) 
     {
         alert("You are winner!\n\t" + "Lets try again\t");
-<<<<<<< HEAD
-=======
 
->>>>>>> 2c1db683410d3ac61f98935647c50188279e8403
     }
 
     else if (resultGame.target.getAttribute('data-title') != "pic" + prgNumber)
@@ -39,11 +33,7 @@ document.onclick = function(resultGame)
         alert("Sorry, you not guess the Nubmer! Try again.\t");
     } 
 
-<<<<<<< HEAD
-    else (resultGame.target.tagName('IMG') == "IMG")
-=======
     else (document.target.getElementsByTagName("IMG") == "IMG")
->>>>>>> 2c1db683410d3ac61f98935647c50188279e8403
     {
         resultGame.target.classList.add('bordered');
         reloadPage();

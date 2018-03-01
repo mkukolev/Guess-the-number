1b725688e668fdb54ae32a0fd2c998222324e600
Mikhail <michael.kukolev.phone@gmail.com>
2018-03-01 21:50:39 +0300

    add difficulty
    easy
    normal
    hard

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ function calc() {
     return randomNum;
 }
 var resNum = calc.apply();
-//debug
+
+//debug#1
 console.log(resNum)
 
 rollResult = function (myevent) {
     if (resNum == resNum) {
         Num = Math.floor((Math.random() * 10) + 1);
         resNum = Num;
+//debug#2
         console.log(resNum);
         
     }
@@ -20,57 +22,78 @@ rollResult = function (myevent) {
  $("#StartButton").click(function () {
     $("#SplashScreen").hide();
     $("#Game:hidden").show();
-    $("#text:hidden").show();
+    $("#text:hidden").show(); 
+    //$("#DiffButton:hidden").show();
+}); 
+
+
+//Difficulty in work
+
+/* $("#StartButton").click(function () {
+    $("#StartButton").hide();
+    $("#EasyButton:hidden").show();
+    $("#NormalButton:hidden").show();
+    $("#HardButton:hidden").show();
+}); */
+
+/* $("#EasyButton").click(function () {
 });
 
+$("#NormalButton").click(function () {
+});
+
+$("#HardButton").click(function () {
+}); */
+
+//Modal windows
 var modalwin = document.getElementById('myWin');
 var modallos = document.getElementById('myLos');
 var modalover = document.getElementById('myOver');
 
+//Buttons' in windos for WIN
 var modbtn = document.getElementsByClassName('buttons')[0];
 var spanWin = document.getElementsByClassName("close")[0];
 var spanTry = document.getElementsByClassName("exit")[0];
-
+//Button's in windows for TRY
 var spanClose = document.getElementsByClassName("close")[1];
 var spanExit = document.getElementsByClassName("exit")[1];
-
+//Button in windows for Overv
 var spanOver = document.getElementsByClassName("over")[0];
-//ar spanExit = document.getElementsByClassName("exit")[2];
-
+//Open modal windows
 modbtn.onclick = function() {d
     modalwin.style.display = "block";
     modallos.style.display = "block";
     modalover.style.display = "block";
-
 }
+
 //
 //
-//
-/////WIN
+///// WIN OK!
 spanWin.onclick = function() {
     modalwin.style.display = "none";
 }
-
+///// WIN EXIT
 spanTry.onclick = function() {
     modalwin.style.display = reloadPage() ;
 }
 
-/////TRY
+///// TRY OK!
 spanClose.onclick = function() {
     modallos.style.display = "none";
     }
-
+///// TRY EXIT
 spanExit.onclick = function() {
 modallos.style.display = reloadPage() ;
 }
 
-/////OVER
+/////OVER RESTART GAME
 spanOver.onclick = function() {
     modalover.style.display = reloadPage();
 }
 //
 //
-//
+
+
 function reloadPage() {
     setTimeout (function ()
     {
@@ -92,14 +115,16 @@ var butlist = [
 ];
 
 var elemPress = document.getElementsByClassName("buttons")[0];
-//var mybut = "mybutton";
+
 for (var i in butlist) {
      var butts = '<button id = "' + butlist[i]  + '" > Number ' + (Number(i) + 1) + ' </button>';
     elemPress.innerHTML += butts; 
 }
 document.createElement.butts;
+//difficult count's
+//var count = [1, 3, 5];
 
-var count = 3;
+var count = 5;
 var point = 0;
 var tryResult = document.getElementById('try').innerHTML = "The tries:" + count;
 var pntResult = document.getElementById('pnt').innerHTML = "You're points:" + point;
